1e0cdc85f9a2ad75c6b05fd7768070e5498589d6
Burhan YILMAZ <wburhany@gmail.com>
2020-04-13 11:37:48 +0300

    Photos action updated.

--- actions/PhotosAction.js
+++ actions/PhotosAction.js
@@ -1,7 +1,8 @@
 import { fetchPhotos } from "by-jsonplaceholderapi";
-import { setPhotosError, setPhotos } from "../slices/PhotosSlice";
+import { setPhotosError, setPhotos, changeLoading } from "../slices/PhotosSlice";
 
 export const getPhotos = () => (dispatch) => {
+  dispatch(changeLoading(true));
   fetchPhotos({
     failureCallback: (error) => dispatch(setPhotosError({error, errorMessage: 'Failure'})),
     successCallback: (data) => dispatch(setPhotos(data)),
